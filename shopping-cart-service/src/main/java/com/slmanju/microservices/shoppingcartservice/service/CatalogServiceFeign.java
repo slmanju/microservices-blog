23a15f64c6093f406140500b3ce061acac211fea
Manjula Jayawardana <manjulajayawardana@gmail.com>
2019-03-23 23:11:22 +0530

    add ribbon load balancer

--- shopping-cart-service/src/main/java/com/slmanju/microservices/shoppingcartservice/service/CatalogServiceFeign.java
+++ shopping-cart-service/src/main/java/com/slmanju/microservices/shoppingcartservice/service/CatalogServiceFeign.java
@@ -1,11 +1,14 @@
 package com.slmanju.microservices.shoppingcartservice.service;
 
 import com.slmanju.microservices.shoppingcartservice.service.view.Product;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "catalog-service", url = "http://localhost:8010")
+@FeignClient(name = "catalog-service")
+@RibbonClient(name = "catalog-service")
+//@FeignClient(name = "catalog-service", url = "http://localhost:8010")
 public interface CatalogServiceFeign {
 
     @GetMapping("/{id}")
